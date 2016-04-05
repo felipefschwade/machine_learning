dcaba809ce22d86d14610cfafbe82a927b53ba4a
Felipe <felipefaeschwade@gmail.com>
2016-04-04 23:04:40 -0400

    Acompanhamento aula 1 - Video 5

--- classificacao.py
+++ classificacao.py
@@ -10,10 +10,14 @@ dados = [porco1, porco2, porco3, cachorro1, cachorro2, cachorro3]
 
 marcacoes = [1, 1, 1, -1, -1, -1]
 
-misterioso = [1, 1, 1]
-
+misterioso1 = [1, 1, 1]
+misterioso2 = [1, 0, 0]
+misterioso3 = [0, 0, 1]
+teste = [misterioso1, misterioso2, misterioso3]
 from sklearn.naive_bayes import MultinomialNB
-
+teste_esperado = [-1, 1, -1]
 modelo = MultinomialNB(alpha=1.0, class_prior=None, fit_prior=True)
 modelo.fit(dados, marcacoes)
-print(modelo.predict(misterioso))
+print(modelo.predict(teste))
+resultado = modelo.score(teste, teste_esperado)
+print resultado
