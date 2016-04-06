dfe3b12e3cd8c4719a0f070281c0a23944a74df5
Felipe <felipefaeschwade@gmail.com>
2016-04-05 23:25:11 -0400

    Acompanhamento da Aula 2 - Video 1 - Abrindo e explorando um CSV

---
+++ classifica_acessos.py
@@ -0,0 +1,6 @@
+#Importa do arquivo dados.py a função acessos (pode ser classe)
+from dados import carregarAcessos
+#Assimila X e Y ao resultaado da função
+X, Y = carregarAcessos()
+print X
+print Y
---
+++ dados.py
@@ -0,0 +1,19 @@
+#Importa a biblioteca para trabalhar com CSV
+import csv
+
+def carregarAcessos():
+	X = []
+	Y = []
+
+	#Abre o arquivo no modo "r"ead "b"inarie
+	arquivo = open('acesso_pagina.csv', 'rb')
+	#utiliza a função reader para assimilar o arquivo ao que for lido
+	lido = csv.reader(arquivo)
+	lido.next() #devolve o proximo item partindo do objeto
+	for linha in lido:
+		 #seleciona apenas os valores de acessou_home, como_funciona e acessou_contato
+		dado = [int(linha[0]), int(linha[1]), int(linha[2])]
+		X.append(dado)
+		#seleciona apenas o referente a comprou
+		Y.append(int(linha[3])) 
+	return X, Y
