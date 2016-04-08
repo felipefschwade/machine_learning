7112842cb1ed205df294a6c8aab30a0816e86b13
Felipe <felipefaeschwade@gmail.com>
2016-04-08 00:12:47 -0400

    Commit que eu esqueci

--- classifica_acessos.py
+++ classifica_acessos.py
@@ -2,8 +2,13 @@
 from dados import carregarAcessos
 #assimila X e Y ao resultaado da funcao
 X, Y = carregarAcessos()
-
+treino_X = X[:90]
+treino_Y = Y[:90]
+teste_X = X[-9:]
+teste_Y = Y[-9:]
 from sklearn.naive_bayes import MultinomialNB
 modelo = MultinomialNB()
-modelo.fit(X, Y)
-print modelo.predict([[1,0,1], [0, 1, 0]])
+modelo.fit(treino_X, treino_Y)
+print modelo.predict(teste_X)
+taxa_de_acerto = 100.0 * modelo.score(teste_X, teste_Y)
+print taxa_de_acerto
--- dados.py
+++ dados.py
@@ -7,12 +7,28 @@ def carregarAcessos():
 
 	#Abre o arquivo no modo "r"ead "b"inarie
 	arquivo = open('acesso_pagina.csv', 'rb')
-	#utiliza a funcai reader para assimilar o arquivo ao que for lido
+	#utiliza a funcao reader para assimilar o arquivo ao que for lido
 	lido = csv.reader(arquivo)
 	lido.next() #devolve o proximo item partindo do objeto
 	for linha in lido:
 		 #seleciona apenas os valores de acessou_home, como_funciona e acessou_contato
 		dado = [int(linha[0]), int(linha[1]), int(linha[2])]
+		X.append(dado)
+		#seleciona apenas o referente a comprou
+		Y.append(int(linha[3])) 
+	return X, Y
+
+def carregarBuscas():
+	X = []
+	Y = []
+
+	#Abre o arquivo no modo "r"ead "b"inarie
+	arquivo = open('buscas.csv', 'rb')
+	lido = csv.reader(arquivo)
+	lido.next() #devolve o proximo item partindo do objeto
+	for linha in lido:
+		 #seleciona apenas os valores de nome, busca e logado
+		dado = [int(linha[0]), linha[1], int(linha[2])]
 		X.append(dado)
 		#seleciona apenas o referente a comprou
 		Y.append(int(linha[3])) 
