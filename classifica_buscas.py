1352a7d98b8583f827cd72612179398bb07485b7
Felipe <felipefaeschwade@gmail.com>
2016-04-13 23:33:15 -0400

    Acompanhamento da aula 4

--- classifica_buscas.py
+++ classifica_buscas.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import pandas as pd #Utiliza a biblioteca pandas de Data Analysis
 df = pd.read_csv('buscas.csv') #Df = data_frame
 Y_df = df['comprou'] #O pandas torna o cabecaalho como referencia ao dado, nao necessitando referencia numerica
@@ -12,14 +13,13 @@ treino_X = X[:tamanho_de_treino]
 treino_Y = Y[:tamanho_de_treino]
 teste_X = X[-tamanho_de_teste:]
 teste_Y = Y[-tamanho_de_teste:]
-#Testando a eficacia de um algoritmo que chuta tudo 0 ou 1
-acerto_um = len(Y[Y==1])
-acerto_zero = len(Y[Y==0])
-taxa_de_acerto_base = 100.0 * max(acerto_um, acerto_zero) / len(Y)
-print "Taxa de acerto base: %f" % taxa_de_acerto_base
 #importa a biblioteca do modelo bayesiano
 from sklearn.naive_bayes import MultinomialNB
 modelo = MultinomialNB()
 modelo.fit(treino_X, treino_Y) #realiza o treino #realiza o teste
 taxa_de_acerto = 100.0 * modelo.score(teste_X, teste_Y) #calcula a taxa de acerto
 print "Taxa de acerto do algoritmo: %f" % taxa_de_acerto
+#Testando a eficacia de um algoritmo que chuta tudo 0 ou 1
+acerto_base = max(Counter(teste_Y).itervalues())
+taxa_de_acerto_base = 100.0 * acerto_base / len(teste_Y)
+print "Taxa de acerto base: %f" % taxa_de_acerto_base
